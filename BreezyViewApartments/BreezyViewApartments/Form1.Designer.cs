2ea1b7064b0d4cf7df56534bda25552f92f032c2
Douglas Masho <douglasmasho@gmail.com>
2021-08-10 22:21:23 +0200

    finished attempt successfully

--- BreezyViewApartments/BreezyViewApartments/Form1.Designer.cs
+++ BreezyViewApartments/BreezyViewApartments/Form1.Designer.cs
@@ -29,11 +29,24 @@ namespace BreezyViewApartments
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbBedrooms = new System.Windows.Forms.ListBox();
+            this.lbBathrooms = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbLake = new System.Windows.Forms.RadioButton();
+            this.rbStreet = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -41,7 +54,7 @@ namespace BreezyViewApartments
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Ostrich Sans Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(79, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 48);
             this.label1.TabIndex = 0;
@@ -51,7 +64,7 @@ namespace BreezyViewApartments
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Ostrich Sans Black", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(78, 107);
+            this.label2.Location = new System.Drawing.Point(79, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(308, 53);
             this.label2.TabIndex = 1;
@@ -62,7 +75,7 @@ namespace BreezyViewApartments
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Ostrich Sans Bold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.Blue;
-            this.label3.Location = new System.Drawing.Point(225, 160);
+            this.label3.Location = new System.Drawing.Point(239, 107);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(161, 86);
             this.label3.TabIndex = 2;
@@ -70,25 +83,166 @@ namespace BreezyViewApartments
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(444, 107);
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(406, 96);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(626, 370);
+            this.pictureBox1.Size = new System.Drawing.Size(766, 490);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ostrich Sans Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(32, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(310, 48);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Number of Bedrooms";
+            // 
+            // lbBedrooms
+            // 
+            this.lbBedrooms.FormattingEnabled = true;
+            this.lbBedrooms.ItemHeight = 25;
+            this.lbBedrooms.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.lbBedrooms.Location = new System.Drawing.Point(84, 310);
+            this.lbBedrooms.Name = "lbBedrooms";
+            this.lbBedrooms.Size = new System.Drawing.Size(180, 104);
+            this.lbBedrooms.TabIndex = 5;
+            // 
+            // lbBathrooms
+            // 
+            this.lbBathrooms.FormattingEnabled = true;
+            this.lbBathrooms.ItemHeight = 25;
+            this.lbBathrooms.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.lbBathrooms.Location = new System.Drawing.Point(90, 493);
+            this.lbBathrooms.Name = "lbBathrooms";
+            this.lbBathrooms.Size = new System.Drawing.Size(180, 104);
+            this.lbBathrooms.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Ostrich Sans Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(12, 442);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(330, 48);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Number of Bathrooms";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Ostrich Sans Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(130, 628);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 48);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "View";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbLake);
+            this.groupBox1.Controls.Add(this.rbStreet);
+            this.groupBox1.Location = new System.Drawing.Point(42, 718);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 150);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // rbLake
+            // 
+            this.rbLake.AutoSize = true;
+            this.rbLake.Location = new System.Drawing.Point(6, 82);
+            this.rbLake.Name = "rbLake";
+            this.rbLake.Size = new System.Drawing.Size(72, 29);
+            this.rbLake.TabIndex = 1;
+            this.rbLake.TabStop = true;
+            this.rbLake.Text = "Lake";
+            this.rbLake.UseVisualStyleBackColor = true;
+            // 
+            // rbStreet
+            // 
+            this.rbStreet.AutoSize = true;
+            this.rbStreet.Location = new System.Drawing.Point(6, 47);
+            this.rbStreet.Name = "rbStreet";
+            this.rbStreet.Size = new System.Drawing.Size(82, 29);
+            this.rbStreet.TabIndex = 0;
+            this.rbStreet.TabStop = true;
+            this.rbStreet.Text = "Street";
+            this.rbStreet.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Ostrich Sans Medium", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(545, 651);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(181, 35);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Total(p/m):";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Ostrich Sans Black", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTotal.Location = new System.Drawing.Point(714, 628);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(183, 115);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "1000";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.Color.Blue;
+            this.btnCalc.Font = new System.Drawing.Font("Ostrich Sans Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalc.ForeColor = System.Drawing.Color.White;
+            this.btnCalc.Location = new System.Drawing.Point(84, 928);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(163, 64);
+            this.btnCalc.TabIndex = 12;
+            this.btnCalc.Text = "Time To Move!";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1184, 670);
+            this.ClientSize = new System.Drawing.Size(1184, 1122);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lbBathrooms);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbBedrooms);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,6 +254,17 @@ namespace BreezyViewApartments
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lbBedrooms;
+        private System.Windows.Forms.ListBox lbBathrooms;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbLake;
+        private System.Windows.Forms.RadioButton rbStreet;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnCalc;
     }
 }
 
--- BreezyViewApartments/BreezyViewApartments/Form1.cs
+++ BreezyViewApartments/BreezyViewApartments/Form1.cs
@@ -17,5 +17,58 @@ namespace BreezyViewApartments
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            getTotal();
+        }
+
+        public void getTotal()
+        {
+            //get the selected from the first listBox
+            double bedroomNum = Convert.ToDouble(lbBedrooms.SelectedItem.ToString());
+
+            switch (bedroomNum)
+            {
+                case 1:
+                    myGlobals.totalCost += 450;
+                    break;
+                case 2:
+                    myGlobals.totalCost += 550;
+                    break;
+                case 3:
+                    myGlobals.totalCost += 700;
+                    break;
+                default:
+                    myGlobals.totalCost += 0;
+                    break;
+            }
+
+            double bathroomNum = Convert.ToDouble(lbBathrooms.SelectedItem.ToString());
+            if(bathroomNum > 1)
+            {
+                myGlobals.totalCost += 75;
+            }
+
+            string selectedBtn = groupBox1.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked).Name;
+            System.Diagnostics.Debug.WriteLine(selectedBtn);
+            if(selectedBtn == "rbLake")
+            {
+                myGlobals.totalCost += 50;
+            }
+            lblTotal.Text = Convert.ToString(myGlobals.totalCost);
+            //reset total cost
+            myGlobals.totalCost = 0;
+        }
     }
+}
+
+
+public static class myGlobals
+{
+    public static double totalCost = 0;
 }
