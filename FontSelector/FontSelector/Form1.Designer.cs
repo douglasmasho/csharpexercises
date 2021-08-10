addffc6d282f8cf2e218c91902b86fa857de117c
Douglas Masho <douglasmasho@gmail.com>
2021-08-10 21:33:25 +0200

    finished the font selector

---
+++ FontSelector/FontSelector/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace FontSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFontFamily = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbFontSize = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.helloLbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbFontFamily
+            // 
+            this.lbFontFamily.BackColor = System.Drawing.Color.Gainsboro;
+            this.lbFontFamily.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbFontFamily.FormattingEnabled = true;
+            this.lbFontFamily.ItemHeight = 25;
+            this.lbFontFamily.Items.AddRange(new object[] {
+            "Poppins",
+            "Montserrat",
+            "Oswald",
+            "Product Sans",
+            "Bahnschrift"});
+            this.lbFontFamily.Location = new System.Drawing.Point(85, 258);
+            this.lbFontFamily.Name = "lbFontFamily";
+            this.lbFontFamily.Size = new System.Drawing.Size(180, 50);
+            this.lbFontFamily.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Product Sans", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(85, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pick a font family";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbFontSize
+            // 
+            this.lbFontSize.BackColor = System.Drawing.Color.Gainsboro;
+            this.lbFontSize.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbFontSize.FormattingEnabled = true;
+            this.lbFontSize.ItemHeight = 25;
+            this.lbFontSize.Items.AddRange(new object[] {
+            "16",
+            "20",
+            "24",
+            "26",
+            "30"});
+            this.lbFontSize.Location = new System.Drawing.Point(497, 258);
+            this.lbFontSize.Name = "lbFontSize";
+            this.lbFontSize.Size = new System.Drawing.Size(180, 50);
+            this.lbFontSize.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Product Sans", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(497, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pick a font size";
+            // 
+            // helloLbl
+            // 
+            this.helloLbl.AutoSize = true;
+            this.helloLbl.Location = new System.Drawing.Point(331, 77);
+            this.helloLbl.Name = "helloLbl";
+            this.helloLbl.Size = new System.Drawing.Size(24, 25);
+            this.helloLbl.TabIndex = 4;
+            this.helloLbl.Text = "...";
+            this.helloLbl.Click += new System.EventHandler(this.helloLbl_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Product Sans", 9.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(325, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 44);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Say Hello";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.helloLbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbFontSize);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbFontFamily);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbFontFamily;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbFontSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label helloLbl;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FontSelector/FontSelector/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FontSelector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fam = lbFontFamily.SelectedItem.ToString();
+            string size = lbFontSize.SelectedItem.ToString();
+            SayHello(fam, size);    
+        }
+
+        private void SayHello(string family, string size)
+        {
+            //create a list 
+            List<string> fontList = new List<string>();
+            //populate the list
+            fontList.Add(family);
+            fontList.Add(size);
+
+            //convert the list to an array
+            string[] fontArr = fontList.ToArray();
+
+            //join the list as a string with a comma seperator
+            string fontStr = String.Join(", ", fontArr);
+
+                // Create the FontConverter.
+             System.ComponentModel.TypeConverter converter = 
+            System.ComponentModel.TypeDescriptor.GetConverter(typeof(Font));
+            System.Diagnostics.Debug.WriteLine(fontStr);
+
+            //create a font object using the font converter with the string
+            Font selectedFont = (Font)converter.ConvertFromString(fontStr);
+
+            //make helloLbl say hello
+            helloLbl.Text = "Hello";
+
+            //change the font of helloLbl
+            helloLbl.Font = selectedFont;
+
+        }
+
+        private void helloLbl_Click(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
+
+
+public static class MyGlobals
+{
+    public static string fontFamily = "";
+    public static string fontSize = "";
+}
